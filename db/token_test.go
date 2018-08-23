083a2bc967ede21765bdc101ee4df5656d0ab7ca
范杨 <fanyang.coder@gmail.com>
2018-08-23 11:48:41 +0800

    fix db interface test err by asset

--- db/token_test.go
+++ db/token_test.go
@@ -21,8 +21,8 @@ func initTest(t *testing.T) {
 		Database: "test3",
 	})
 
-	token.Reg(types.NewAsset("main", 0, eos.EOSSymbol).Symbol)
-	token.Reg(types.NewAsset("side", 0, eos.EOSSymbol).Symbol)
+	token.Reg(types.NewAssetFromEosforce("main", 0, eos.EOSSymbol).Symbol)
+	token.Reg(types.NewAssetFromEosforce("side", 0, eos.EOSSymbol).Symbol)
 
 	CreateAccount(accountName, accountChain)
 }
@@ -31,7 +31,7 @@ func initTest(t *testing.T) {
 func TestAccountToken(t *testing.T) {
 	initTest(t)
 	for i := 0; i < 4; i++ {
-		err := AddToken(accountName, accountChain, types.NewAsset("main", 1111111, eos.EOSSymbol))
+		err := AddToken(accountName, accountChain, types.NewAssetFromEosforce("main", 1111111, eos.EOSSymbol))
 		if err != nil {
 			t.Errorf("add token err by %s", err.Error())
 			t.FailNow()
@@ -40,7 +40,7 @@ func TestAccountToken(t *testing.T) {
 	}
 
 	for i := 0; i < 4; i++ {
-		err := AddToken(accountName, accountChain, types.NewAsset("main", 1111111, eos.EOSSymbol))
+		err := AddToken(accountName, accountChain, types.NewAssetFromEosforce("main", 1111111, eos.EOSSymbol))
 		if err != nil {
 			t.Errorf("add token err by %s", err.Error())
 			t.FailNow()
@@ -60,7 +60,7 @@ func TestAccountToken(t *testing.T) {
 	}
 
 	for i := 0; i < 4; i++ {
-		err := CostToken(accountName, accountChain, types.NewAsset("main", 11112, eos.EOSSymbol))
+		err := CostToken(accountName, accountChain, types.NewAssetFromEosforce("main", 11112, eos.EOSSymbol))
 		if err != nil {
 			t.Errorf("cost token err by %s", err.Error())
 			t.FailNow()
