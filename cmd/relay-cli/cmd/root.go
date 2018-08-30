dc3ee578c73842db68d28e6575e1027b2b5d59ad
范杨 <fanyang.coder@gmail.com>
2018-08-30 16:42:51 +0800

    add cfgs and init client to chains

--- cmd/relay-cli/cmd/root.go
+++ cmd/relay-cli/cmd/root.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/eosforce/relay/chain/wallets"
+	"github.com/eosforce/relay/cmd/config"
+
 	"github.com/cihub/seelog"
+	"github.com/eosforce/relay/token"
+	"github.com/eosforce/relay/types"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -25,8 +30,9 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	RootCmd.PersistentFlags().StringP("log-cfg", "", "./log_cfg.xml", "seelog cfg file")
+	RootCmd.PersistentFlags().StringP("cfg", "", "./cfg.json", "chain cfg file")
 
-	for _, flag := range []string{"log-cfg"} {
+	for _, flag := range []string{"log-cfg", "cfg"} {
 		if err := viper.BindPFlag(flag, RootCmd.PersistentFlags().Lookup(flag)); err != nil {
 			panic(err)
 		}
@@ -40,9 +46,47 @@ func init() {
 		}
 		seelog.ReplaceLogger(logger)
 	}
+
+	initConfig()
 }
 
 func initConfig() {
+	// TODO By FanYang user def symbol
+	token.Reg(types.Symbol{
+		Precision: 4,
+		Chain:     "main",
+		Symbol:    "EOS",
+	})
+	token.Reg(types.Symbol{
+		Precision: 4,
+		Chain:     "side",
+		Symbol:    "EOS",
+	})
+	token.Reg(types.Symbol{
+		Precision: 4,
+		Chain:     "side",
+		Symbol:    "SYS",
+	})
+	token.Reg(types.Symbol{
+		Precision: 4,
+		Chain:     "main",
+		Symbol:    "TST",
+	})
+
+	var cfg config.RelayCfg
+	configFilePath := viper.GetString("cfg")
+	err := config.LoadJsonCfg(configFilePath, &cfg)
+	if err != nil {
+		seelog.Errorf("load cfg err by %s", err.Error())
+		return
+	}
+
+	// init wallets
+
+	wallets.Get().RegWallet(wallets.NewWalletInfoFromCfg(&cfg.Main))
+	for _, sideCfg := range cfg.Side {
+		wallets.Get().RegWallet(wallets.NewWalletInfoFromCfg(&sideCfg))
+	}
 }
 
 // Exists reports whether the named file or directory exists.
